6f2f9aac73cf80313f15d3978c156ee9d9a9c629
A01423328 <a01423328@itesm.mx>
2024-04-16 17:19:38 -0600

    add kinematic model of puzzlebot node

---
+++ localisation/scripts/puzzlebot_kinematic_model.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python
+import rospy
+import numpy as np
+from std_msgs.msg import Float32
+from geometry_msgs.msg import Twist
+
+#Declare Variables to be used
+# pose = JointState()
+msgRobot = Twist()
+#WHEEL VAR
+R = 5
+WHEELBASE = 19
+L = 16
+
+
+def callback_cmd(msg):
+    global msgRobot
+    msgRobot.linear.x = msg.linear.x
+    msgRobot.angular.z = msg.angular.z
+
+
+#Getting Position - SOLVER
+def pose_obtainal(lin_vel, ang_vel):
+    ang = ang_vel * np.sin(ang_vel)
+    x_vel = lin_vel * np.cos(ang)
+    y_vel = lin_vel * np.sin(ang)
+
+    return x_vel,y_vel
+   
+#DIFFERENTIAL DRIVE MODEL
+def diff_model(lin_vel, ang_vel):
+   #!One wheel is twice as fast as other wheel
+   # wr = 2*wl
+   # wl = 1/2 wr
+#    wr = (ang*L) / (3*R)
+#    wl = (2*ang*L) / (3*R)
+
+    # wr = (2*lin_vel) / (R)
+    # # wl = - (2*lin_vel) / (R) 
+    # wl = (2*lin_vel) / (R) - 2*wr
+
+    wl = ((2 * lin_vel) - (L * ang_vel)) / (2 * R)
+    wr = ((2 * lin_vel) / R) - wl
+
+    # print (wl,"/",wr)
+
+    return wr, wl
+   
+   
+
+if __name__=="__main__":
+    rospy.init_node("puzzlebot_kinematic_model")
+
+    #Setup de publishers
+    #   pose_pub = rospy.Publisher("pose", Float32 , queue_size=10)
+    wr_pub = rospy.Publisher("wr", Float32 , queue_size=10)
+    wl_pub = rospy.Publisher("wl", Float32 , queue_size=10)
+    #Set Subscriber
+    rospy.Subscriber("cmd_vel", Twist , callback=callback_cmd)
+
+
+    pose_pub = rospy.Publisher("pose", Twist , queue_size=10)
+
+    # rate = rospy.get_time()
+    rate = rospy.Rate(rospy.get_param("~node_rate",100))
+
+    #Run the node
+    try:
+        while not rospy.is_shutdown():
+            
+            # cmd_sub = rospy.Subscriber("wr", Twist, msgRobot)
+            #rospy.spin() #last line without while
+
+            linear_vel = msgRobot.linear.x
+            angular_vel = msgRobot.angular.z
+
+            # print("Values gotten linear: ", linear_vel)
+            # print("Values gotten angular: ", angular_vel)
+
+            wr,wl = diff_model(linear_vel, angular_vel)
+
+            print("Values of right wheel: ", wr)
+            print("Values of left wheel: ", wl)
+
+            wr_pub.publish(wr)
+            wl_pub.publish(wl)
+            
+            # x_dot, y_dot = pose_obtainal(linear_vel,angular_vel)
+            # msgRobot.linear.x = x_dot
+            # msgRobot.linear.y = y_dot
+            # msgRobot.angular.z = angular_vel
+        #   pose.header.stamp = rospy.Time.now()
+        #   pose.name = ["pose"]
+        #   pose.positionX = [x]
+        #   pose.positionY = [y]
+
+
+        #   pose_sub.publish(pose)
+
+            rate.sleep()
+    except rospy.ROSInterruptException:
+        pass #Initialize and Setup node
